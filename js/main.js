2f909d5811edd2b67ebfdc923b97a86202adafcb
Ketrin <geleczkak26@gmail.com>
2021-08-25 15:16:56 +0300

    Add random colors

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const board = document.querySelector('#board');
-const colors = ['yellow', 'blue', 'green', 'red', 'purple'];
-const SQUARES_NUMBER = 500;
+const colors = ['#FA8072', '#ADFF2F', '#FF1493', '#008B8B', '#00FFFF', '#FFA500', '#4682B4', '#BA55D3', 
+'#7B68EE', '#FF4500', '#87CEFA', '#FFFF00'];
+const SQUARES_NUMBER = 1000;
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div');
@@ -17,10 +18,12 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
 function setColor(element) {
     const color = getRandomColor();
     element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
 function removeColor(element) {
     element.style.backgroundColor = '#1d1d1d';
+    element.style.boxShadow = '0 0 2px black';
 }
 
 function getRandomColor() {
